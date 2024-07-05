7d0edc48e8619ec5e12d2302ecf4638201bcca81
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-07-05 17:17:43 +0100

    random target and hit count

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ pygame.display.set_caption('Shooting range')
 icon = pygame.image.load("img/Shooting_Range_Game_Icon.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load('img/target.png')
+image_list = ['img/target.png', 'img/klipartz.com (1).png', 'img/klipartz.com (2).png',
+              'img/klipartz.com (3).png', 'img/klipartz.com (4).png', 'img/klipartz.com (5).png']
+target_img = pygame.image.load(random.choice(image_list))
 target_width = 80
 target_height = 80
 
@@ -20,6 +22,7 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+hit_count = 0
 
 running = True
 while running:
@@ -33,6 +36,12 @@ while running:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
                 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                target_img = pygame.image.load(random.choice(image_list))
+                hit_count += 1
+
+    font = pygame.font.Font(None, 36)
+    text = font.render('Hits: ' + str(hit_count), True, (0, 0, 0))
+    screen.blit(text, (10, 10))
 
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
